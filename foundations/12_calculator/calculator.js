6cdd63ecdc0c3e872e92b981ef98923a41f40323
Daniel Nowak <danieln311@gmail.com>
2026-04-29 13:17:31 +0000

    homework for later

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((acc, curr) => {
+    return acc + curr;
+  }, 0)
 };
 
 const multiply = function() {
-
+  return array.reduce((acc, curr) => {
+    return acc * curr;
+  }, 0)
 };
 
-const power = function() {
-	
+const power = function(base, expo) {
+	return base ** expo;
 };
 
-const factorial = function() {
-	
+const factorial = function(facto) {
+  let total = 1;
+	for (i = 1; i < facto; i++) {
+    total = total * (i + 1)
+  }
+  return total;
 };
 
 // Do not edit below this line
