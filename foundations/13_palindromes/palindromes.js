ffb37ad939858df1735c7ec77768a596a93e41c5
Daniel Nowak <danieln311@gmail.com>
2026-04-30 12:47:15 +0000

    Add solutions to exercises

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (input) {
+  let alphabetAndNumbers = "abcdefghijklmnopqrstuvwxyz0123456789".split("")
 
+  let filteredInput = input.toLowerCase().split("").filter(letter => alphabetAndNumbers.includes(letter))
+  // console.log(filteredInput)
+  let reversed = input.toLowerCase().split("").filter(letter => alphabetAndNumbers.includes(letter)).reverse();
+  
+  return reversed.join("") == filteredInput.join("")
 };
 
 // Do not edit below this line
